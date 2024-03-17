bc1422888984993a30c6183e8ef0b0ea6e02a888
ICHINOSE Shogo <shogo82148@gmail.com>
2024-03-18 00:00:08 +0900

    first implement

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/shogo82148/go-mysql-pool
+
+go 1.22.1
+
+require github.com/go-sql-driver/mysql v1.8.0
+
+require filippo.io/edwards25519 v1.1.0 // indirect
---
+++ pool.go
@@ -0,0 +1,135 @@
+package mysqlpool
+
+import (
+	"context"
+	"crypto/rand"
+	"database/sql"
+	"fmt"
+	"sync"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+type Pool struct {
+	// MySQLConfig is the configuration for the MySQL connection.
+	MySQLConfig *mysql.Config
+
+	// DDL is Data Definition.
+	DDL string
+
+	mu      sync.Mutex
+	adminDB *sql.DB
+	freeDB  []*sql.DB
+}
+
+func (p *Pool) Get(ctx context.Context) (*sql.DB, error) {
+	p.mu.Lock()
+	if len(p.freeDB) > 0 {
+		l := len(p.freeDB)
+		db := p.freeDB[l-1]
+		p.freeDB = p.freeDB[:l-1]
+		p.mu.Unlock()
+		return db, nil
+	}
+	p.mu.Unlock()
+	return p.new(ctx)
+}
+
+func (p *Pool) Put(db *sql.DB) {
+	p.mu.Lock()
+	p.freeDB = append(p.freeDB, db)
+	p.mu.Unlock()
+}
+
+func (p *Pool) Close() error {
+	p.mu.Lock()
+	for _, db := range p.freeDB {
+		db.Close()
+	}
+	if p.adminDB != nil {
+		p.adminDB.Close()
+	}
+	p.mu.Unlock()
+	return nil
+}
+
+func (p *Pool) new(ctx context.Context) (*sql.DB, error) {
+	dbName, err := p.createDB(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := p.initDB(ctx, dbName); err != nil {
+		return nil, err
+	}
+
+	// Open a new connection to the database.
+	cfg := p.MySQLConfig.Clone()
+	cfg.DBName = dbName
+	conn, err := mysql.NewConnector(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return sql.OpenDB(conn), nil
+}
+
+// createDB creates a new database and returns the name of the database created.
+func (p *Pool) createDB(ctx context.Context) (string, error) {
+	adminDB, err := p.getAdminDB()
+	if err != nil {
+		return "", err
+	}
+
+	var buf [8]byte
+	_, err = rand.Read(buf[:])
+	if err != nil {
+		return "", err
+	}
+	dbName := fmt.Sprintf("test_%x", buf)
+	if _, err := adminDB.ExecContext(ctx, fmt.Sprintf("CREATE DATABASE `%s`", dbName)); err != nil {
+		return "", err
+	}
+	return dbName, nil
+}
+
+func (p *Pool) initDB(ctx context.Context, dbName string) error {
+	// Open a new connection to the database.
+	cfg := p.MySQLConfig.Clone()
+	cfg.DBName = dbName
+	cfg.MultiStatements = true
+	conn, err := mysql.NewConnector(cfg)
+	if err != nil {
+		return err
+	}
+	db := sql.OpenDB(conn)
+	defer db.Close()
+
+	// Execute the DDL.
+	_, err = db.ExecContext(ctx, p.DDL)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *Pool) getAdminDB() (*sql.DB, error) {
+	// If adminDD is already created, return it.
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if p.adminDB != nil {
+		db := p.adminDB
+		return db, nil
+	}
+
+	// Create a new adminDB.
+	cfg := p.MySQLConfig.Clone()
+	cfg.DBName = ""
+	cfg.MultiStatements = true
+	conn, err := mysql.NewConnector(cfg)
+	if err != nil {
+		return nil, err
+	}
+	db := sql.OpenDB(conn)
+	p.adminDB = db
+	return db, nil
+}
